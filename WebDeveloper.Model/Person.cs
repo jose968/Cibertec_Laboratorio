a630f4fe3685dc84ade6f0d3d3acd1e6a61a2aa1
Jose Morales <ulises9688@gmail.com>
2016-07-24 14:00:04 -0500

    Clases 24 julio 2016, Entity , Formulario - Tabla

---
+++ WebDeveloper.Model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDeveloper.Model
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
