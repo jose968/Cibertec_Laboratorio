89d364496a22420202734c79b8797946ee150ba0
Luis Enrique <luchoarrieta@hotmail.com>
2016-09-11 13:52:05 -0500

    Testing

---
+++ WebDeveloper.Tests/Controllers/PersonControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDeveloper.Areas.Personnel.Controllers;
+using Xunit;
+using FluentAssertions;
+using System.Web.Mvc;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Tests.Controllers
+{
+    public class PersonControllerTest
+    {
+        private PersonController controller;
+
+        public PersonControllerTest()
+        {
+            controller = new PersonController();
+        }
+
+        [Fact(DisplayName = "ListActionWithEmptyParameters")]
+        public void ListActionWithEmptyParameters()
+        {
+            var result = controller.List(null, null) as PartialViewResult;
+            result.ViewName.Should().Be("_List");
+
+            var modelCount = (IEnumerable<Person>)result.Model;
+            modelCount.Count().Should().Be(15);
+        }
+
+
+    }
+}
